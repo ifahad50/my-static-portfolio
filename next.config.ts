8fc16784da114bf24e6c5f0e0a3d6be4f9066f9b
Fahad Iqbal <fahad@isswa.com.au>
2025-02-21 11:17:14 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	basePath: '/my-static-portfolio',
+	output: 'export',
+}
 
-export default nextConfig;
+export default nextConfig
