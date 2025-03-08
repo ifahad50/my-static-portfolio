bb1bd70e045138c03c82e2caa67d2f2d04e08858
ifahad50dev <ifahad50dev@gmail.com>
2025-03-08 17:11:36 +0800

    added image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,23 +9,20 @@ function HeroSection() {
 	return (
 		<div className='flex flex-col items-center justify-start md:justify-center space-y-6 h-screen relative '>
 			<div className='flex flex-col items-center justify-center space-y-2'>
-				<Heading
-					text={siteSettings.mainTitle}
-					className='!my-2 !mt-10 md:!mt-0'
-				/>
+				<div className='flex flex-col items-center justify-center space-y-2 mt-10 mb-4'>
+					<img
+						src='/header_image.jpeg'
+						alt='Fahad Iqbal'
+						className='w-[100px] h-[100px] object-cover rounded-lg shadow-2xl'
+					/>
+				</div>
+				<Heading text={siteSettings.mainTitle} />
 				<SubHeading text={`( ${siteSettings.position} )`} />
 			</div>
 			<p className='text-white text-center md:text-start md:text-lg max-w-2xl'>
 				{siteSettings.coverDescription}
 			</p>
 			<ContactBlocks />
-			<div className='flex flex-col items-center justify-center space-y-2'>
-				<img
-					src='/header_image.jpeg'
-					alt='Fahad Iqbal'
-					className='w-[100px] h-[100px] object-cover rounded-lg shadow-2xl'
-				/>
-			</div>
 		</div>
 	)
 }
